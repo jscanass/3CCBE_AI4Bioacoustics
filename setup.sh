209dbf9d2f988796c4dbde8f82fd35cc0e4f18c7
Juan Cañas <jscanass@gmail.com>
2026-09-25 08:58:29 -0500

    Upate setup file with new paths

--- setup.sh
+++ setup.sh
@@ -5,12 +5,14 @@ uv pip install "wavio>=0.0.9"
 uv pip install "batdetect2==1.0.8"
 
 # Download data
-gdown 1UWPoP6J9bh9l7Q-cRYsd2I6rrQGnjMUJ
+gdown 1lBvKnORB-1wUcZtac59O_fwS3JRanM-a
 
 # Unzip data
-unzip acoustic_data.zip
+unzip 3ccbe_data.zip
 
 # Download utility modules
-wget https://github.com/mbsantiago/AI-Intervene-Training-Material/raw/refs/heads/main/BioacousticsAI/evaluation_utils.py
-wget https://github.com/mbsantiago/AI-Intervene-Training-Material/raw/refs/heads/main/BioacousticsAI/plotting_utils.py
-wget https://github.com/mbsantiago/AI-Intervene-Training-Material/raw/refs/heads/main/BioacousticsAI/audio_utils.py
+wget https://github.com/jscanass/3CCBE_AI4Bioacoustics/raw/refs/heads/main/evaluation_utils.py
+wget https://github.com/jscanass/3CCBE_AI4Bioacoustics/raw/refs/heads/main/plotting_utils.py
+wget https://github.com/jscanass/3CCBE_AI4Bioacoustics/raw/refs/heads/main/audio_utils.py
+wget https://github.com/jscanass/3CCBE_AI4Bioacoustics/raw/refs/heads/main/utils.py
+
